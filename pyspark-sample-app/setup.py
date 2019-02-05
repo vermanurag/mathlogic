250f58db7b9fb8d8614586c16af37043aceb8fa5
Anurag Verma <vermanurag@users.noreply.github.com>
2019-02-05 06:16:21 +0530

    Create setup.py

---
+++ pyspark-sample-app/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyspark-sample-app',
+      version='0.1',
+      description='Sample Pyspark App',
+      url='',
+      author='Anurag Verma',
+      author_email='anurag.verma@fnmathlogic.com',
+      license='All Rights Reserved',
+      packages=['pyspark-sample-app'],
+      zip_safe=False)
